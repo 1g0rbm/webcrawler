ed5bc3d5774da97ca466f4c4729772c58222eebc
1g0rbm <m1g0rb89@gmail.com>
2017-11-20 01:00:03 +0700

    Parsing info for one parser

--- src/ParsingManager.php
+++ src/ParsingManager.php
@@ -5,7 +5,9 @@ namespace Ig0rbm\Webcrawler;
 use Ig0rbm\HandyBag\HandyBag;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\OutputArgument;
 
 /**
  * @package Ig0rbm\Webcrawler
@@ -32,22 +34,52 @@ class ParsingManager extends Command
         $this
             ->setName('parser:info')
             ->setDescription('Show info about all registered parsers.')
-            ->setHelp('This command allows you to find out information about all regisered parsers.');
+            ->setHelp('This command allows you to find out information about all regisered parsers.')
+            ->addArgument('parsername', InputArgument::OPTIONAL, 'Name of the parser');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $writeln = [
-            'ParserManager',
-            '============='
+        $stdOut = [
+            '<comment>ParserManager</comment>',
+            '<comment>=============</comment>'
         ];
 
-        foreach ($this->parsers->getAll() as $name => $value) {
-            $writeln[] = sprintf('name: %s', $value->getName());
-            $writeln[] = sprintf('status: %s', $value->getStatus());
-            $writeln[] = sprintf('=============');
+        if ($input->getArgument('parsername')) {
+            $stdOut = $this->statsByName($input->getArgument('parsername'), $stdOut);
+        } else {
+            $stdOut = $this->allStats($stdOut);
         }
 
-        $output->writeln($writeln);
+        $output->writeln($stdOut);
+    }
+
+    protected function statsByName(string $name, array $stdOut)
+    {
+        if (!$this->parsers->has($name)) {
+            $stdOut[] = sprintf('<error>Parser with name "%s" is not registered.</error>', $name);
+        } else {
+            $stdOut[] = sprintf('<info>name:</info> %s', $this->parsers->get($name)->getName());
+            $stdOut[] = sprintf('<info>status:</info> %s', $this->parsers->get($name)->getStatus());
+        }
+
+        $stdOut[] = sprintf('<comment>=============</comment>');
+
+        return $stdOut;
+    }
+
+    /**
+     * @param array $stdOut
+     * @return array
+     */
+    protected function allStats(array $stdOut)
+    {
+        foreach ($this->parsers->getAll() as $name => $value) {
+            $stdOut[] = sprintf('<info>name:</info> %s', $value->getName());
+            $stdOut[] = sprintf('<info>status:</info> %s', $value->getStatus());
+            $stdOut[] = sprintf('<comment>=============</comment>');
+        }
+
+        return $stdOut;
     }
 }
