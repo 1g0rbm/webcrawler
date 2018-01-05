5ad7507baa4a9af9ea8bd1c8ab8bff698de847f8
1g0rbm <m1g0rb89@gmail.com>
2018-01-05 23:12:51 +0700

    Added parser builder

---
+++ src/ParserBuilder.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Ig0rbm\Webcrawler;
+
+class ParserBuilder
+{
+    private $domain;
+    private $name;
+    private $rootNamespace;
+    private $chain = [];
+
+    /**
+     * @param array $chain
+     * 
+     * @return $this
+     */
+    public function setChain(array $chain)
+    {
+        $this->chain = $chain;
+
+        return $this;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getChain()
+    {
+        return $this->chain;
+    }
+
+    /**
+     * @param string $rootNamespace
+     * 
+     * @return $this
+     */
+    public function setRootNamespace(string $rootNamespace)
+    {
+        $this->rootNamespace = $rootNamespace;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRootNamespace()
+    {
+        return $this->rootNamespace;
+    }
+
+    /**
+     * @param string $name
+     * 
+     * @return $this
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $domain
+     * 
+     * @return $this
+     */
+    public function setDomain(string $domain)
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+    /**
+     * @return \ArrayIterator
+     */
+    public function getChainIterator()
+    {
+        return new \ArrayIterator($this->chain);
+    }
+    /**
+     * @param \Closure $callback
+     */
+    public function chainWalk(\Closure $callback)
+    {
+        $cit = $this->getChainIterator();
+        while ($cit->valid()) {
+            $callback($cit->key(), $cit->current());
+            $cit->next();
+        }
+    }
+}
